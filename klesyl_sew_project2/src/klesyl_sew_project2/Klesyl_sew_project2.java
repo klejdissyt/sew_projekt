f4982b7b6d7b9202082376b65dd6e66fabf92bd7
klejdis sylia <klesyl15@htl-shkoder.com>
2020-02-26 10:56:56 +0100

    neues NetBeans Projekt angelegt

---
+++ klesyl_sew_project2/src/klesyl_sew_project2/Klesyl_sew_project2.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package klesyl_sew_project2;
+
+/**
+ *
+ * @author klesyl15
+ */
+public class Klesyl_sew_project2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
